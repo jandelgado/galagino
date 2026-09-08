a6f0865799e3c7000fab9466304a7e19e0255291
galagino <galagino@galagino>
2026-09-08 17:04:39 +0000

    actually we don't need to filter, all tiles are +1

--- source/src/machines/burgertime/burgertime.cpp
+++ source/src/machines/burgertime/burgertime.cpp
@@ -383,6 +383,7 @@ void burgertime::blit_bg_tile(short row, int idx) {
 // senza serve un layer pixel-preciso separato (il carattere e' comunque
 // sulla stessa griglia da 8px).
 static inline bool burgertime_is_ingredient_char(unsigned short code) {
+  return true;
   return ((code < 336) || (code >= 512 && code <= 895));
 }
 
